e13b5c0db82986a5da1f2cff1c680ae5e4b211df
Shubham Goyal <122973606+shubham-goyal-0811@users.noreply.github.com>
2023-05-29 00:05:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let sg = document.getElementById('fb');
 sg.onclick = function() {
   let postUrl = encodeURI(document.location.href); 
-  window.open(`https://www.facebook.com/sharer.php?u=${postUrl}`);
+  window.open(`https://www.facebook.com/sharer.php?u=${postUrl}`, 'targetWindow',`toobar=no,location=no,status=no,menubar=no,scrollbars=yes,resizeable=yes,width=SomeSize,height=SomeSize`);
 };
 
 let t = document.getElementById('tw');
 t.onclick = function() {
   let postUrl = encodeURI(document.location.href); 
   let postTitle = encodeURI("Hi everyone, please check this out: ");
-  window.open(`https://twitter.com/share?url=${postUrl}&text=${postTitle}`);
+  window.open(`https://twitter.com/share?url=${postUrl}&text=${postTitle}`,'targetWindow',`toobar=no,location=no,status=no,menubar=no,scrollbars=yes,resizeable=yes,width=SomeSize,height=SomeSize`);
 };
 
 let lin = document.getElementById('siddham');
 lin.onclick = function() {
   let postUrl = encodeURI(document.location.href); 
   let postTitle = encodeURI("Hi everyone, please check this out: ");
-  window.open(`https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`);
+  window.open(`https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`,'targetWindow',`toobar=no,location=no,status=no,menubar=no,scrollbars=yes,resizeable=yes,width=SomeSize,height=SomeSize`);
 };
 let wp = document.getElementById('shubham');
 wp.onclick = function() {
   let postUrl = encodeURI(document.location.href); 
   let postTitle = encodeURI("Hi everyone, please check this out: ");
-  window.open(`https://wa.me/?text=${postTitle} ${postUrl}`);
+  window.open(`https://wa.me/?text=${postTitle} ${postUrl}`,'targetWindow',`toobar=no,location=no,status=no,menubar=no,scrollbars=yes,resizeable=yes,width=SomeSize,height=SomeSize`);
 };
 document.addEventListener("DOMContentLoaded", function() {
   var ser = document.getElementById("sear11");
